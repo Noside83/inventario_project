88f4c0af9bb1d4591aa698aa3e45ab88f9e9684b
Noside83 <ehsolucionesinformaticas@gmail.com>
2025-07-20 22:21:41 -0500

    Se implemento tokens en api DRF con simple jwt

--- inventario/api/views.py
+++ inventario/api/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 from inventario.models import Producto
 from inventario.api.serializer import ProductoSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class ProductoViewSet(viewsets.ModelViewSet):
+    #implementando la autenticación con JWT
+    permission_classes =[IsAuthenticated]
+
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
 
--- inventario_project/settings.py
+++ inventario_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles', 
     'rest_framework',
+    'rest_framework_simplejwt',
     'inventario',
 ]
 
@@ -132,3 +133,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK ={
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
